90cb851b26efb8c9d2224779011c3a467f047c75
Chuck <chuckleong21@gmail.com>
2024-06-03 10:02:57 +0800

    Add label validation

--- app/shiny/validate_label.R
+++ app/shiny/validate_label.R
@@ -34,7 +34,7 @@ filter_by_number <- function(data) {
 }
 
 check_real_column <- function(cross_tbl) {
-  test_nm <- c("etiketki", "xing_hao", "pai_zi", "yong_tu_can_shu_cai_zhi")
+  test_nm <- c("name", "model", "brand", "material")
   all(test_nm %in% colnames(reduce(cross_tbl$real, tibble))[-1])
 }
 
@@ -47,16 +47,20 @@ check_label <- function(field, cross_tbl) {
   
   switch(field,
          "item_no" = sapply(cross_tbl$item_no, \(i) ifelse(item_fields["item_no"] == i, 0, 1)),
-         "name" = sapply(.real$etiketki, \(i) tidy_stringdist(tidy_comb(item_fields["name"], i))[, "jw"]),
-         "model" = sapply(.real$xing_hao, \(i) ifelse(str_detect(i, item_fields["model"]), 0, 1)),
-         "brand" = sapply(.real$pai_zi, \(i) {
-           if(str_detect(i, "нет", negate = TRUE)) {
-             if(str_detect(i, item_fields["brand"])) 0 else 1
+         "name" = sapply(.real$name, \(i) tidy_stringdist(tidy_comb(item_fields["name"], i))[, "jw"]),
+         "model" = sapply(.real$model, \(i) ifelse(is.na(i), -1, ifelse(str_detect(i, item_fields["model"]), 0, 1))),
+         "brand" = sapply(.real$brand, \(i) {
+           if(is.na(i)) {
+             -1
            } else {
-             if(str_detect(item_fields["brand"], "отсутствует")) 0 else 1
+             if(str_detect(i, "нет", negate = TRUE)) {
+               if(str_detect(i, item_fields["brand"])) 0 else 1
+             } else {
+               if(str_detect(item_fields["brand"], "отсутствует")) 0 else 1
+             }
            }
          }), 
-         "material" = sapply(.real$yong_tu_can_shu_cai_zhi, \(i) {
+         "material" = sapply(.real$material, \(i) {
            a <- ifelse(str_detect(i, item_fields["material"]), 0, 1)
            b <- tidy_stringdist(tidy_comb(item_fields["material"], i))[, "jw"]
            (a + b) / 2 
@@ -72,6 +76,7 @@ check_labels <- function(cross_tbl, ignore.warn = TRUE) {
   } else  {
     d <- sapply(fields, \(i) check_label(i, cross_tbl = cross_tbl))
   }
+  names(d)[1:5] <- fields 
   d %>% 
     as_tibble() %>% 
     unnest(everything()) %>% 
@@ -79,88 +84,24 @@ check_labels <- function(cross_tbl, ignore.warn = TRUE) {
     tibble::rowid_to_column("row")
 }
 
-highlight_row <- function(check_tbl) {
-  r <- check_tbl %>% 
-    pull(dissimilarity) %>% 
-    min_rank()
-  which(r == 1)
-}
-
-
-box_series <- "../Emma/static/OT012.xlsx"
-labels <- "../Emma/static/маркировка ОТ012.xlsx"
-
-labels_data <- readxl::read_excel(labels, sheet = "Маркировки")
-labels_data <- map(seq(0, ncol(labels_data) - 2, 2), \(i) labels_data[, 1:2 + i]) %>% 
-  map(\(i) {
-    set_names(i, c("name", "value")) %>% 
-      pivot_wider(names_from = "name", values_from = "value")
-  }) %>% 
-  list_rbind()
-
-test_data <- labels_data %>% 
-  select(1:5) %>% 
-  janitor::clean_names() %>% 
-  tibble::rowid_to_column("id") %>% 
-  rename(item_no = "nomer_gruza", 
-         etietki = "naimenovanie", 
-         xing_hao = "model_artikul", 
-         pai_zi = "brend", 
-         yong_tu_can_shu_cai_zhi = "material") %>% 
-  mutate(
-    number = str_replace(item_no, "(.+)(\\((.+)\\))", "\\3"),
-    number = ifelse(number == item_no, str_replace_all(number, "(.+)\\-(\\d+)", "1\\-\\2"), number),
-    number = str_replace_all(number, "(\\d+)\\-(\\d+)", "\\1:\\2"),
-    item_no = str_remove(item_no, "\\(.+\\)"), 
-    xing_hao = str_trim(xing_hao)
-  ) %>% 
-  relocate(number, .after = item_no)
-
-real_data <- readxl::read_excel(box_series) %>%
-  slice(seq_len(nrow(.) - 3)) %>% 
-  janitor::clean_names() %>% 
-  fill(c(item_no:yong_tu_can_shu_cai_zhi), .direction = "down") %>% 
-  select(item_no, etiketki, number, pai_zi:yong_tu_can_shu_cai_zhi) %>% 
-  tibble::rowid_to_column("id") %>% 
-  mutate(id = id + 1) %>% 
-  mutate(number = str_replace_all(number, "\\-", ":"), 
-         number = map(number, \(x) eval(parse(text = x)))) %>% 
-  nest(real = c(id, etiketki:yong_tu_can_shu_cai_zhi)) %>% 
-  right_join(test_data, join_by("item_no"))
-
-real_data <- real_data %>% 
-  left_join(real_data %>% 
-              group_by(item_no) %>% 
-              do(nest(filter_by_number(.))), 
-            join_by(item_no)) %>% 
-  group_by(item_no) %>% 
-  mutate(group_id = row_number(), 
-         real = pmap(list(real, data, group_id), \(x = real, y = data, id = group_id) {
-           x[na.omit(y[, id, drop = TRUE]), ]
-         })) %>% 
-  mutate(disimilarity = map(id, \(id = id) check_labels(.[id, ])))
-
-# list_rbind(real_data$disimilarity) %>% 
-#   pull(dissimilarity) %>% 
-#   log() %>% 
-#   hist(breaks = 5)
-
-highlight_tbl <- function(id) {
-  highlight_colors <- real_data$disimilarity[[id]] %>% 
-    select(-1, -2) %>% 
-    map(\(x, y) {
+highlight_tbl <- function(crosstbl, id) {
+  highlight_colors <- crosstbl$disimilarity[[id]] %>% 
+    select(-1) %>% 
+    map(\(x) {
       case_when(
-        x <= 0.3 ~ "#198754", 
+        x >= 0 & x <= 0.3 ~ "#198754", 
         x > 0.3 & x <= 0.4 ~ "#ffc107", 
-        .default = "#dc3545"
+        x > 0.4 ~ "#dc3545",
+        x < 0 ~ "gray25"
       )
     })
   
   green <- map(highlight_colors, \(color) which(color == "#198754"))
   yellow <- map(highlight_colors, \(color) which(color == "#ffc107"))
   red <- map(highlight_colors, \(color) which(color == "#dc3545"))
+  gray <- map(highlight_colors, \(color) which(color == "gray25"))
   
-  real_data$real[[id]] %>% 
+  crosstbl$real[[id]] %>% 
     select(-number) %>% 
     gt() %>% 
     tab_style(
@@ -176,10 +117,11 @@ highlight_tbl <- function(id) {
         cell_text(color = "#fff")
       ), 
       locations = list(
-        cells_body(column = etiketki, rows = green$name), 
-        cells_body(column = pai_zi, rows = green$brand),
-        cells_body(column = xing_hao, rows = green$model),
-        cells_body(column = yong_tu_can_shu_cai_zhi, rows = green$material)
+        cells_body(column = item_no, rows = green$item_no), 
+        cells_body(column = name, rows = green$name), 
+        cells_body(column = brand, rows = green$brand),
+        cells_body(column = model, rows = green$model),
+        cells_body(column = material, rows = green$material)
       )
     ) %>% 
     tab_style(
@@ -188,10 +130,11 @@ highlight_tbl <- function(id) {
         cell_text(color = "#000")
       ), 
       locations = list(
-        cells_body(column = etiketki, rows = yellow$name), 
-        cells_body(column = pai_zi, rows = yellow$brand),
-        cells_body(column = xing_hao, rows = yellow$model),
-        cells_body(column = yong_tu_can_shu_cai_zhi, rows = yellow$material)
+        cells_body(column = item_no, rows = yellow$item_no), 
+        cells_body(column = name, rows = yellow$name), 
+        cells_body(column = brand, rows = yellow$brand),
+        cells_body(column = model, rows = yellow$model),
+        cells_body(column = material, rows = yellow$material)
       )
     ) %>% 
     tab_style(
@@ -200,52 +143,160 @@ highlight_tbl <- function(id) {
         cell_text(color = "#fff")
       ), 
       locations = list(
-        cells_body(column = etiketki, rows = red$name), 
-        cells_body(column = pai_zi, rows = red$brand),
-        cells_body(column = xing_hao, rows = red$model),
-        cells_body(column = yong_tu_can_shu_cai_zhi, rows = red$material)
+        cells_body(column = item_no, rows = red$item_no), 
+        cells_body(column = name, rows = red$name), 
+        cells_body(column = brand, rows = red$brand),
+        cells_body(column = model, rows = red$model),
+        cells_body(column = material, rows = red$material)
       )
     ) %>% 
-    opt_stylize(style = 4, color = "gray")
+    tab_style(
+      style = list(
+        cell_fill(color = "gray25"), 
+        cell_text(color = "#fff")
+      ), 
+      locations = list(
+        cells_body(column = item_no, rows = gray$item_no), 
+        cells_body(column = name, rows = gray$name), 
+        cells_body(column = brand, rows = gray$brand),
+        cells_body(column = model, rows = gray$model),
+        cells_body(column = material, rows = gray$material)
+      )
+    ) %>% 
+    sub_missing(missing_text = "-") %>% 
+    tab_options(column_labels.background.color = "gray40")
+}
+
+label_data <- function(file = NULL, sheet = NULL, copy = FALSE, path = paste0(tempdir(), "\\tmp.rds")) {
+  if(!is.null(file) & !is.null(sheet)) {
+    labels_data <- openxlsx2::read_xlsx(file = file, sheet = sheet, skip_empty_rows = TRUE)
+    labels_data[1, ]
+  } else {
+    return(tibble::tibble())
+  }
+  names(labels_data)[which(is.na(names(labels_data)))] <- seq_along(names(labels_data)[which(is.na(names(labels_data)))]) + 0.1
+  labels_data <- map(seq(0, ncol(labels_data) - 2, 2), \(i) labels_data[, 1:2 + i]) %>% 
+    map(\(i) as_tibble(i) %>% set_names(c("name", "value")))
   
+  if(copy) saveRDS(labels_data, file = path)
+  labels_data
 }
 
-walk(seq_len(nrow(real_data)), \(i) {
-  print(highlight_tbl(i))
-  Sys.sleep(2.5)
-})
-
-case_when(
-  test_statistic <= 0.3 ~ "#198754", 
-  test_statistic > 0.3 & test_statistic <= 0.4 ~ "#ffc107", 
-  .default = "#dc3545"
-)
-
-cross_table <- function(.test, .real) {
-  number <- str_remove(.test[1, 2, drop = TRUE], "(?:\\(.+\\))")
-  .real <- subset(.real, str_detect(item_no, number))
-  list(test = .test, real = .real)
+cross_tbl <- function(box_series_path, label_series_path, label_sheet) {
+  
+  test_data <- label_data(file = label_series_path, sheet = label_sheet) %>%
+    map(\(i) i %>%
+          pivot_wider(names_from = "name", values_from = "value")) %>%
+    list_rbind() %>%
+    select(1:5) %>%
+    set_names(c("item_no", "name", "model", "brand", "material")) %>%
+    tibble::rowid_to_column("id") %>%
+    mutate(
+      number = str_replace(item_no, "(.+)(\\((.+)\\))", "\\3"),
+      number = ifelse(number == item_no, str_replace_all(number, "(.+)\\-(\\d+)", "1\\-\\2"), number),
+      number = str_replace_all(number, "(\\d+)\\-(\\d+)", "\\1:\\2"),
+      item_no = str_remove(item_no, "\\(.+\\)"),
+      item_no = str_trim(item_no),
+      model = str_trim(model)
+    ) %>%
+    relocate(number, .after = item_no)
+  
+  real_data <- openxlsx2::read_xlsx(file = box_series_path, sheet = "东线模板", 
+                                    fill_merged_cells = TRUE, skip_empty_rows = TRUE) %>%
+    select(c(1, 3, 4, 7:9)) %>%
+    as_tibble() %>%
+    set_names(c("item_no", "number", "name", "brand", "model", "material")) %>%
+    filter(!is.na(item_no)) %>%
+    tibble::rowid_to_column("id") %>%
+    mutate(id = id + 1,
+           item_no = str_trim(item_no),
+           number = str_replace_all(number, "\\-", ":"),
+           number = map(number, \(x) eval(parse(text = x)))) %>%
+    nest(real = c(id, number:material)) %>% 
+    mutate(real = map(seq_along(real), \(i) real[[i]] %>% 
+                        mutate(item_no = item_no[i]) %>% 
+                        relocate(item_no, .after = number))) %>% 
+    right_join(test_data, join_by("item_no"))
+  
+  real_data <- real_data %>%
+    left_join(real_data %>%
+                group_by(item_no) %>%
+                do(nest(filter_by_number(.))),
+              join_by(item_no)) %>%
+    group_by(item_no) %>%
+    mutate(group_id = row_number(),
+           real = pmap(list(real, data, group_id), \(x = real, y = data, id = group_id) {
+             x[na.omit(y[, id, drop = TRUE]), ]
+           })) %>%
+    mutate(disimilarity = map(id, \(id = id) check_labels(.[id, ])))
+  
+  real_data
 }
 
-
-cross_tbl <- cross_table(labels_data[[44]], box_data)
-# cross_tbl$real <- subset(cross_tbl$real, id != 17)
-r <- check_labels(cross_tbl = cross_tbl) %>% highlight_row()
-
-cross_tbl$real %>%
-  slice(r) %>%
-  gt() %>%
-  tab_style(
-    style = list(
-      cell_fill(color = ifelse(length(r) > 1, "#ffc107", "#198754")),
-      cell_text(color = ifelse(length(r) > 1, "#000", "#fff"))
+validateUI <- function(id, i18n, current_page = NULL, total_page = NULL) {
+  ns <- NS(id)
+  
+  useShinyjs()
+  # label view
+  tagList(
+    fluidRow(
+      column(
+        width = 12, 
+        tableOutput(ns("singleLabel"))
+      )
     ),
-    locations = cells_body(rows = seq_along(r))
-  ) %>%
-  opt_stylize(style = 3, color = "gray")
-# 
-# 
-# check_label("name", labels_data[[1]], box_data)
-# check_labels(labels_data[[14]], box_data) %>% 
-#    pull(dissimilarity) %>% 
-#    min_rank()
+    # pagination
+    fluidRow(
+      column(width = 4,actionButton(ns("prevLabel"), label = i18n$translate("上一个"))), 
+      column(width = 4, htmlOutput(ns("refreshBtn"))), 
+      column(width = 4, actionButton(ns("nextLabel"), label = i18n$translate("下一个")))
+    ),
+    # result view
+    fluidRow(
+      column(width = 12, gt_output(ns("validateResult")))
+    )
+  )
+}
+
+validateServer <- function(id, i18n) {
+  moduleServer(id, function(input, output, session) {
+    ns <- session$ns
+    
+    p <- reactiveVal(1)
+    n <- reactive(length(session$userData$label$data()))
+    
+    output$singleLabel <- renderTable({
+      session$userData$label$data()[[p()]]
+    })
+    
+    output$refreshBtn <- renderUI({
+      actionButton(ns("refreshLabel"), label = sprintf("%d/%d", p(), n()), icon = icon("refresh", style = "color:navyblue"))
+    })
+    
+    observe({
+      toggleState(id = ns("prevLabel"), condition = p() > 1, asis = TRUE)
+    })
+    observe({
+      toggleState(id = ns("nextLabel"), condition = p() < n(), asis = TRUE)
+    })
+    
+    observeEvent(input$refreshLabel, {
+      p(1)
+    })
+    
+    observeEvent(input$prevLabel, {
+      i <- p() - 1
+      p(i)
+    })
+    
+    observeEvent(input$nextLabel, {
+      i <- p() + 1
+      p(i)
+    })
+    
+    output$validateResult <- render_gt({
+      req(session$userData$box$data())
+      highlight_tbl(session$userData$box$data(), p())
+    })
+  })
+}
