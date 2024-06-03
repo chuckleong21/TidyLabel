8a5e8c600ef2d5eddce916cfc5fcc870f66d0150
Chuck <chuckleong21@gmail.com>
2024-06-03 10:00:33 +0800

    Move PDF recognition

--- app/shiny/tidyup2.R
+++ app/shiny/tidyup2.R
@@ -1,3 +1,4 @@
+source("pdf_coord.R")
 get_table_pages <- function(file, updateProgress = NULL) {
   p <- c()
   for(i in seq_len(get_n_pages(file))) {
@@ -13,98 +14,31 @@ get_table_pages <- function(file, updateProgress = NULL) {
   p
 }
 
-get_coordinates <- function(n, version) {
-  if(n == 1) {
-    id <-        switch(
-      version, 
-      "1" = list(list(c(339.2, 347.58, 351.66, 365.78))), 
-      "2" = list(list(c(358.4255, 332.9425, 377.4152, 360.3851)))
-    )
-    hs_code <- switch (version,
-      "1" = list(list(c(336.3251, 385.9036, 351.6561, 452.9770))), 
-      "2" = list(list(c(360.3413, 367.4272, 378.3732, 483.3339)))
-    )
-    weight <- switch (version,
-      "1" = list(list(c(361.24, 452.02, 378.49, 528.67))), 
-      "2" = list(list(c(382.3311, 435.4386, 402.3208, 485.2497)))
-    )
-    tax_code <- switch (version,
-      "1" = list(list(c(507.84, 68.74, 600.79, 90.78))), 
-      "2" = list(list(c(529.72241,  47.48635, 621.68146,  74.30774)))
-    )
-    tax <- switch (version,
-      "1" = list(list(c(506.88, 233.55, 599.83, 307.33))), 
-      "2" = list(list(c(528.7645, 221.8254, 621.6815, 291.7526)))
-    )
-  } else {
-    id <- switch (version,
-      "1" = map(seq_len(3) - 1, \(i) list(c(101.57+166*i, 341.74, 117.86+166*i, 364.87))), 
-      "2" = map(seq_len(3) - 1, \(i) list(c(91.12742 + 161.89*i, 348.26906, 110.15927 + 161.89*i, 375.09044)))
-    )
-    hs_code <- switch (version,
-      "1" = map(seq_len(3) - 1, \(i) list(c(102.53+166*i, 380.07, 115.94+166*i, 431.9))), 
-      "2" = map(seq_len(3) - 1, \(i) list(c(91.12742 + 161.89*i, 385.6291, 110.15927 + 161.89*i, 447.9004)))
-    )
-    weight <- switch (version,
-      "1" = map(seq_len(3) - 1, \(i) list(c(126.48+166*i, 449.14, 141.81+166*i, 526.76))), 
-      "2" = map(seq_len(3) - 1, \(i) list(c(114.1274 + 161.89*i, 442.1441, 133.1593 + 161.89*i, 523.5704)))
-    )
-    tax_code <- switch (version,
-                        "1" = map(seq_len(3), \(i) {
-                          top = 604.62; left = 61.08; bottom = 695.65; right = 89.82
-                          case_match(i, 
-                                     1 ~ list(c(top, left, bottom, right)), 
-                                     2 ~ list(c(top, left + 257.8, bottom, right + 257.8)), 
-                                     3 ~ list(c(top + 119.5, left, bottom + 119.5, right)))
-                        }), 
-                        "2" = map(seq_len(3), \(i) {
-                          top = 585.28100; left = 61.85495; bottom = 676.28214; right = 90.59215
-                          x = 257.677; y = 116.8646 
-                          case_match(i, 
-                                     1 ~ list(c(top,  left, bottom,  right)), 
-                                     2 ~ list(c(top, left + x, bottom, right + x)), 
-                                     3 ~ list(c(top + y,  left, bottom + y,  right)))
-                        })
-    )
-    tax <- switch (version,
-      "1" = map(seq_len(3), \(i) {
-        top = 604.62; left = 230.68; bottom = 695.65; right = 302.54
-        case_match(i, 
-                   1 ~ list(c(top, left, bottom, right)), 
-                   2 ~ list(c(top, left + 257.8, bottom, right + 257.8)), 
-                   3 ~ list(c(top + 117, left, bottom + 117, right)))
-      }), 
-      "2" = map(seq_len(3), \(i) {
-        top = 585.28100; left = 233.3203; bottom = 676.28214; right = 306.1212
-        x = 257.677; y = 116.8646
-        case_match(i, 
-                   1 ~ list(c(top, left, bottom, right)), 
-                   2 ~ list(c(top, left + x, bottom, right + x)), 
-                   3 ~ list(c(top + y, left, bottom + y, right)))
-      })
-    )
-  }
-    list(id = id, hs_code = hs_code, weight = weight, tax_code = tax_code, tax = tax)
-}
-
-tidy_page <- function(file, page, version) {
+tidy_page <- function(file, page, version = NULL) {
+  
   tax_code_dict <- paste0(c(1, 2, 5), "010")
-  coordinates <- get_coordinates(page, version)
+  coordinates <- get_coordinates(file, page, version = version)
   map(coordinates, function(coor) {
     map(coor, function(l) 
       unlist(extract_tables(file = file, page = page, guess = FALSE, area = l, output = "matrix")))
   }) %>% 
     as_tibble() %>% 
     unnest(everything()) %>% 
-    mutate(tax_code = fuzzymatch(tax_code, tax_code_dict), 
-           across(c(weight, tax), ~gsub(",", "\\.", .x)),
-           across(everything(), readr::parse_number), 
-           across(c(id, tax_code), as.integer))
+    mutate(
+      tax_code = map_chr(tax_code, \(x) {
+        d <- which(str_detect(x, tax_code_dict))
+        ifelse(length(d) != 0, tax_code_dict[d], x)
+      }),
+      tax_code = fuzzymatch(tax_code, tax_code_dict),
+      across(c(weight, tax), ~gsub(",", "\\.", .x)),
+      across(everything(), readr::parse_number),
+      across(c(id, tax_code), as.integer)
+    ) %>% 
+    filter(!is.na(tax_code))
 }
 
-
 fuzzymatch <- function(string, dict) {
-  sapply(string, function(x) {
+  res <- sapply(string, function(x) {
     tidystringdist::tidy_comb(x, dict) %>% 
       tidystringdist::tidy_stringdist() %>% 
       select(-hamming, -soundex) %>% 
@@ -118,10 +52,11 @@ fuzzymatch <- function(string, dict) {
       slice_max(n) %>% 
       pull(V1)
   })
+  
+  map_chr(res, \(r) ifelse(length(r) != 1, NA_character_, r))
 }
 
-
-tidyup <- function(file, page = NULL, version, updateProgress = NULL) {
+tidyup <- function(file, page = NULL, version = NULL, updateProgress = NULL) {
   if(!grepl("\\.pdf$", file, ignore.case = TRUE)) {
     x <- as.list(match.call())
     file_supplied <- str_extract(basename(x$file), "\\.(.+)$", group = 1)
